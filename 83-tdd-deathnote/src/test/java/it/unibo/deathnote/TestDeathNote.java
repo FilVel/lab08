c6bc6562ac281897d6e124a10783b0582fe86ab1
Filippo Velli <filippo.velli@studio.unibo.it>
2023-11-21 11:30:38 +0100

    Completed the test file required by the exercise

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -6,8 +6,10 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Assertions;
@@ -16,8 +18,9 @@ class TestDeathNote {
 
     private static final int NUMBER_OF_RULES=13;
     private DeathNote dNote;
-    private String testName = "Hanzo Hasashi";
-    private String testName2 = "Jonathan Joestar";
+    private static final String TEST_NAME = "Hanzo Hasashi";
+    private static final String TEST_NAME_2 = "Jonathan Joestar";
+    private static final int INVALID_DETAILS_TIME = 6000 + 100;
 
     @BeforeEach
     public void obtainBook(){
@@ -55,18 +58,52 @@ class TestDeathNote {
 
     @Test
     public void testWriteName(){
-        assertFalse(this.dNote.isNameWritten(testName));
+        assertFalse(this.dNote.isNameWritten(TEST_NAME));
 
         try {
-            this.dNote.writeName(testName);
+            this.dNote.writeName(TEST_NAME);
         } catch (NullPointerException e) {
             Assertions.fail("A name is indeed used here!");
         }
 
-        assertTrue(this.dNote.isNameWritten(testName));
+        assertTrue(this.dNote.isNameWritten(TEST_NAME));
 
-        assertFalse(this.dNote.isNameWritten(testName2));
+        assertFalse(this.dNote.isNameWritten(TEST_NAME_2));
 
         assertFalse(this.dNote.isNameWritten(""));
     }
+
+
+    @Test
+    void testDeathCause() throws InterruptedException {
+        dNote.writeName(TEST_NAME);
+        assertEquals("", dNote.getDeathCause(TEST_NAME));
+        dNote.writeName(TEST_NAME_2);
+        assertTrue(dNote.writeDeathCause("Vampire attack"));
+        assertEquals("Vampire attack", dNote.getDeathCause(TEST_NAME_2));
+        sleep(50);
+        assertFalse(dNote.writeDeathCause("Spontaneous human combustion"));
+        assertEquals("Vampire attack", dNote.getDeathCause(TEST_NAME_2));
+    }
+
+    @Test
+    void testDeathDetails() throws InterruptedException {
+        dNote.writeName(TEST_NAME);
+        assertEquals("", dNote.getDeathDetails(TEST_NAME));
+        assertTrue(dNote.writeDetails("Killed by Sub-Zero (Bi-Han)"));
+        assertEquals("Killed by Sub-Zero (Bi-Han)", dNote.getDeathDetails(TEST_NAME));
+
+        try {
+            dNote.writeDetails("Killed by D'Vorah");
+            Assertions.fail("Details have already been entered!");
+        } catch (IllegalStateException e) {
+            assertEquals("Details have already been entered!", e.getMessage());
+        }
+
+        dNote.writeName(TEST_NAME_2);
+        sleep(INVALID_DETAILS_TIME);
+        assertFalse(dNote.writeDetails("Killed by his own 'brother'"));
+        assertEquals("", dNote.getDeathDetails(TEST_NAME_2));
+    }
+    
 }
